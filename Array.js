971f15b06a8b64b987953db8ba896175c6eca7d4
Manishbachhane <manishbachhane@gmail.com>
2025-08-27 22:09:57 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+const fruits = ["apple", "banana", "mango", "grapes"];
+console.log(fruits);
+
+var mixed = [1, "apple", true, null];
+console.log(mixed);
+
+let arr = new Array(10); //it create array of 10 empty items
+console.log(arr);
+
+const arr1 = Array.of(1, 2, 3, 4, 5);
+console.log(arr1);
+
+const arr2 = Array.from("hello");
+console.log(arr2);
