db53ef7600b2fdc2895bd50a742f21c1ba4fafa8
Manishbachhane <manishbachhane@gmail.com>
2025-08-27 19:46:33 +0530

    Added Numbers.js file

---
+++ Numbers.js
@@ -0,0 +1,10 @@
+let score = 100;
+console.log(score);
+
+
+const  balance = new Number(500.00);
+
+console.log(balance.toString().length);//it convert number to string
+
+const hundreds = 100000000000;
+console.log(hundreds.toLocaleString("en-IN"));//it convert number to local format
