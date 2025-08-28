ead85da0e2014f95e5abceeb7a1e39269dac786a
Manishbachhane <manishbachhane@gmail.com>
2025-08-28 22:33:57 +0530

    objects in js

---
+++ Objects.js
@@ -0,0 +1,22 @@
+const person = {
+  name: "John",
+  age: 30,
+  isStudent: false
+};
+
+console.log(person.name);
+console.log(person["age"]);
+
+console.log(person.isStudent);
+
+
+const person2 = {
+  name: "Jane",
+  age: 25,
+  isStudent: true
+};
+
+console.log(person2.name);
+console.log(person2["age"]);
+
+console.log(person2.isStudent); 
