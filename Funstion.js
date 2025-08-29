ed8cdfd908f11d0bad595a07b2a54e6069e6c4ed
Manishbachhane <manishbachhane@gmail.com>
2025-08-29 14:44:13 +0530

    Function

---
+++ Funstion.js
@@ -0,0 +1,17 @@
+// Regular Function
+function greet() {
+  console.log("Hello, World!");
+}
+
+// Arrow Function
+const add = (x, y) => x + y;
+
+// Immediately Invoked Function Expression (IIFE)
+(function() {
+  console.log("This is an IIFE");
+})();
+
+// Named Function Expression
+const multiply = function(x, y) {
+  return x * y;
+};
