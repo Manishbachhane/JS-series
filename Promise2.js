41d7e78fad5f648bb466fa70a0f4f308363ae217
Manishbachhane <manishbachhane@gmail.com>
2025-08-31 21:39:19 +0530

    Promise2

---
+++ Promise2.js
@@ -0,0 +1,23 @@
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 1 resolved");
+    }, 1000);
+})
+
+let p2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 2 resolved");
+    }, 2*1000);
+})
+
+let p3 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("Promise 3 resolved");
+    }, 3*1000);
+})
+
+Promise.all([p1,p2,p3]).then((result)=>{
+    console.log(result);
+}).catch((error)=>{
+    console.error(error);
+});
